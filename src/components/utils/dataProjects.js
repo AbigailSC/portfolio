3beecc34422d05069927dc8c4326122f78e54f89
AbigailSc <abigailsarzuri@gmail.com>
2022-10-29 12:13:13 -0300

    feat: agregando nuevos proyectos

--- src/components/utils/dataProjects.js
+++ src/components/utils/dataProjects.js
@@ -1,74 +1,105 @@
-export const projects = [{
-  name: 'Padel App',
-  rol: 'Front end Developer',
-  date: ['Agosto 2022', 'Agosto 2022'],
-  img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1663364922/Captura_de_pantalla_634_yar4k2.png',
-  deployLink: 'https://padelapp.netlify.app/',
-  tecnologies: ['React', 'Redux toolkit', 'Chakra UI'],
-  description: 'We developed as a group and from the Front end a shift reservation application for paddle tennis courts.'
-},
-{
-  name: 'Api Food',
-  rol: 'Full Stack Developer',
-  date: ['Julio 2022', 'Julio 2022'],
-  img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1662403896/Captura_de_pantalla_587_hjpsi0.png',
-  deployLink: 'https://pifood-henry-abigailsc.vercel.app/',
-  tecnologies: ['React', 'Redux', 'CSS', 'Express', 'PostgreSQL', 'Sequelize'],
-  description: 'Develop a healthy food app that includes: Search, health level sorting and filtering, recipe creation, etc.'
-},
-{
-  name: 'Weather app',
-  rol: 'Front end Developer',
-  date: ['Mayo 2022', 'Junio 2022'],
-  img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1661885598/ca2_fgjn7m.png',
-  deployLink: 'https://weatherapp-xi-nine.vercel.app/',
-  tecnologies: ['React', 'CSS', 'HTML'],
-  description: 'Develop a weather query application that includes: Search by city and detailed query of each city.'
-},
-{
-  name: 'MusiMarket',
-  rol: 'Front end Developer',
-  date: ['Sep 2022', 'Actualidad'],
-  img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1664319490/Captura_de_pantalla_659_cctxta.png',
-  deployLink: 'https://musimarket.vercel.app/',
-  tecnologies: ['CSS', 'HTML'],
-  description: 'Landing page develop in HTML and CSS about a e-commerce'
-}
+export const projects = [
+  {
+    name: 'GamesBA',
+    rol: 'Front end Developer',
+    date: ['Oct 2022', 'Oct 2022'],
+    img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1667055548/Captura_de_pantalla_690_b6ivy0.png',
+    deployLink: 'https://gamesba.vercel.app/',
+    tecnologies: ['CSS', 'HTML'],
+    id: 4,
+    description: 'Landing page develop in HTML and CSS about a e-commerce about videogames'
+  },
+  {
+    name: 'MusiMarket',
+    rol: 'Front end Developer',
+    date: ['Sep 2022', 'Oct 2022'],
+    img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1664319490/Captura_de_pantalla_659_cctxta.png',
+    deployLink: 'https://musimarket.vercel.app/',
+    tecnologies: ['CSS', 'HTML'],
+    id: 3,
+    description: 'Landing page develop in HTML and CSS about a e-commerce, full responsive'
+  },
+  {
+    name: 'Padel App',
+    rol: 'Front end Developer',
+    date: ['Agosto 2022', 'Agosto 2022'],
+    img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1663364922/Captura_de_pantalla_634_yar4k2.png',
+    deployLink: 'https://padelapp.netlify.app/',
+    tecnologies: ['React', 'Redux toolkit', 'Chakra UI'],
+    id: 2,
+    description: 'We developed as a group and from the Front end a shift reservation application for paddle tennis courts.'
+  },
+  {
+    name: 'Api Food',
+    rol: 'Full Stack Developer',
+    date: ['Julio 2022', 'Julio 2022'],
+    img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1662403896/Captura_de_pantalla_587_hjpsi0.png',
+    deployLink: 'https://pifood-henry-abigailsc.vercel.app/',
+    tecnologies: ['React', 'Redux', 'CSS', 'Express', 'PostgreSQL', 'Sequelize'],
+    id: 1,
+    description: 'Develop a healthy food app that includes: Search, health level sorting and filtering, recipe creation, etc.'
+  },
+  {
+    name: 'Weather app',
+    rol: 'Front end Developer',
+    date: ['Mayo 2022', 'Junio 2022'],
+    img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1661885598/ca2_fgjn7m.png',
+    deployLink: 'https://weatherapp-xi-nine.vercel.app/',
+    tecnologies: ['React', 'CSS', 'HTML'],
+    id: 0,
+    description: 'Develop a weather query application that includes: Search by city and detailed query of each city.'
+  }
 ]
 
-export const projectsReverse = [{
-  name: 'MusiMarket',
-  rol: 'Front end Developer',
-  date: ['Sep 2022', 'Actualidad'],
-  img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1664319490/Captura_de_pantalla_659_cctxta.png',
-  deployLink: 'https://musimarket.vercel.app/',
-  tecnologies: ['CSS', 'HTML'],
-  description: 'Landing page develop in HTML and CSS about a e-commerce'
-},
-{
-  name: 'Weather app',
-  rol: 'Front end Developer',
-  date: ['Mayo 2022', 'Junio 2022'],
-  img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1661885598/ca2_fgjn7m.png',
-  deployLink: 'https://weatherapp-xi-nine.vercel.app/',
-  tecnologies: ['React', 'CSS', 'HTML'],
-  description: 'Develop a weather query application that includes: Search by city and detailed query of each city.'
-},
-{
-  name: 'Api Food',
-  rol: 'Full Stack Developer',
-  date: ['Julio 2022', 'Julio 2022'],
-  img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1662403896/Captura_de_pantalla_587_hjpsi0.png',
-  deployLink: 'https://pifood-henry-abigailsc.vercel.app/',
-  tecnologies: ['React', 'Redux', 'CSS', 'Express', 'PostgreSQL', 'Sequelize'],
-  description: 'Develop a healthy food app that includes: Search, health level sorting and filtering, recipe creation, etc.'
-},
-{
-  name: 'Padel App',
-  rol: 'Front end Developer',
-  date: ['Agosto 2022', 'Agosto 2022'],
-  img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1663364922/Captura_de_pantalla_634_yar4k2.png',
-  deployLink: 'https://padelapp.netlify.app/',
-  tecnologies: ['React', 'Redux toolkit', 'Chakara UI'],
-  description: 'We developed as a group and from the Front end a shift reservation application for paddle tennis courts.'
-}]
+export const projectsReverse = [
+  {
+    name: 'Weather app',
+    rol: 'Front end Developer',
+    date: ['Mayo 2022', 'Junio 2022'],
+    img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1661885598/ca2_fgjn7m.png',
+    deployLink: 'https://weatherapp-xi-nine.vercel.app/',
+    tecnologies: ['React', 'CSS', 'HTML'],
+    id: 0,
+    description: 'Develop a weather query application that includes: Search by city and detailed query of each city.'
+  },
+  {
+    name: 'Api Food',
+    rol: 'Full Stack Developer',
+    date: ['Julio 2022', 'Julio 2022'],
+    img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1662403896/Captura_de_pantalla_587_hjpsi0.png',
+    deployLink: 'https://pifood-henry-abigailsc.vercel.app/',
+    tecnologies: ['React', 'Redux', 'CSS', 'Express', 'PostgreSQL', 'Sequelize'],
+    id: 1,
+    description: 'Develop a healthy food app that includes: Search, health level sorting and filtering, recipe creation, etc.'
+  },
+  {
+    name: 'Padel App',
+    rol: 'Front end Developer',
+    date: ['Agosto 2022', 'Agosto 2022'],
+    img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1663364922/Captura_de_pantalla_634_yar4k2.png',
+    deployLink: 'https://padelapp.netlify.app/',
+    tecnologies: ['React', 'Redux toolkit', 'Chakara UI'],
+    id: 2,
+    description: 'We developed as a group and from the Front end a shift reservation application for paddle tennis courts.'
+  },
+  {
+    name: 'MusiMarket',
+    rol: 'Front end Developer',
+    date: ['Sep 2022', 'Actualidad'],
+    img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1664319490/Captura_de_pantalla_659_cctxta.png',
+    deployLink: 'https://musimarket.vercel.app/',
+    tecnologies: ['CSS', 'HTML'],
+    id: 3,
+    description: 'Landing page develop in HTML and CSS about a e-commerce'
+  },
+  {
+    name: 'GamesBA',
+    rol: 'Front end Developer',
+    date: ['Oct 2022', 'Oct 2022'],
+    img: 'https://res.cloudinary.com/dbhb8sohh/image/upload/v1667055548/Captura_de_pantalla_690_b6ivy0.png',
+    deployLink: 'https://gamesba.vercel.app/',
+    tecnologies: ['CSS', 'HTML'],
+    id: 4,
+    description: 'Landing page develop in HTML and CSS about a e-commerce about videogames'
+  }
+]
