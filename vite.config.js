775b398800ac17f3046e10d4d7abfc4b646b36ef
AbigailSc <abigailsarzuri@gmail.com>
2023-04-07 15:31:17 -0300

    change domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://abigailsc.github.io/portfolio/',
+  base: 'https://abigailsc.dev',
 })
